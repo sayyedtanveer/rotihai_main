003654680b5374de37f2abc2bdebbe76e57f775b
Tanveer <sayyedtanveer1410@gmail.com>
2026-01-04 17:27:10 +0530

    saving

--- server/index.ts
+++ server/index.ts
@@ -430,10 +430,11 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  await setupVite(app, server);
+} else if (process.env.SERVE_CLIENT === "true") {
+  serveStatic(app);
+}
+
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
